44d62c6a1cb56e71ae741a29aea8696988436bb2
asherqu <asherqu@tencent.com>
2025-12-03 11:57:08 +0800

    test

--- pkg/auth/token.go
+++ pkg/auth/token.go
@@ -63,7 +63,7 @@ func (auth *TokenAuthSetterVerifier) SetNewWorkConn(newWorkConnMsg *msg.NewWorkC
 
 func (auth *TokenAuthSetterVerifier) VerifyLogin(m *msg.Login) error {
 	if !util.ConstantTimeEqString(util.GetAuthKey(auth.token, m.Timestamp), m.PrivilegeKey) {
-		return fmt.Errorf("token in login doesn't match token from configuration %s",auth.token)
+		return fmt.Errorf("token in login doesn't match token from configuration token: %s over",auth.token)
 	}
 	return nil
 }
